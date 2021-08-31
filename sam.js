fda0d3d9167f685da7f20c62b1fc3f518f784da0
samuelBabebe <samuel.abebe@miu.edu>
2021-08-31 17:54:28 -0500

    Update sam.js

--- sam.js
+++ sam.js
@@ -1,18 +1,21 @@
-function calcDownpayment(housecost){
-    let downpayment;
-    housecost<50000 ? downpayment = 0.05*housecost :
-    housecost>=50000 && housecost<100000 ? downpayment =
-     2500 + 0.1*(housecost-50000) :
-    housecost>=100000 && housecost<=200000 ? downpayment =
-     7500 + 0.15 *(housecost - 100000) :
-     downpayment = 5000 + 0.1 *(housecost -200000);
-     return downpayment;
+function sumdigts(input){
+    let sum = 0;
+    while(input){
+        sum+=input %10;
+        input = Math.floor(input/ 10);
+    }
+    return sum;
 }
-console.log("expect 2000: ", calcDownpayment(40000));
-console.log("expect 2500: ", calcDownpayment(50000));
-console.log("expect 7500: ", calcDownpayment(100000));
-console.log("expect 22500: ", calcDownpayment(200000));/* here i include <= sign
-on line 6, so that it will include 200000, but in the question i need exclude it 
-from there. i just do that to get what you expected.*/
+function multidigts(input){
+    let multi = 1;
+    while(input){
+        multi*= input%10;
+        input = Math.floor(input/10);
+    }
+    return multi;
+}
+console.log(sumdigts(1234),multidigts(1234));
+console.log(sumdigts(102),multidigts(102));
+console.log(sumdigts(8),multidigts(8));
 
 
