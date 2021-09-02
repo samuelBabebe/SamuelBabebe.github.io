5ab7924e764092ef95063fe19b7a973bba79152a
samuelBabebe <samuel.abebe@miu.edu>
2021-09-02 00:00:38 -0500

    calcdown payment with exported module

--- mocha.js
+++ mocha.js
@@ -1,33 +1,19 @@
-"use strict";
-const functionmodule = require("./learn.js");
-const computeSalesCommission = functionmodule.computeSalesCommission;
+"use strict"
+const functionmodule = require("./sam.js")
+const calcDownpayment = functionmodule.calcDownpayment;
 const assert = require("assert");
-describe("test of ComputSalesCommission", function(){
-    it("tests salaried and 200 sales", function(){
-    assert.strictEqual(computeSalesCommission(true, 200), 0);
+describe("cheak the house price and",function(){
+    it(" tests house cost is 40000", function(){
+        assert.strictEqual(calcDownpayment(40000), 2000);
     });
-    it("tests not salaried and 200 sales", function(){
-    assert.strictEqual(computeSalesCommission(false, 200), 0);
+    it(" tests house cost is 50000", function(){
+        assert.strictEqual(calcDownpayment(50000), 2500);
     });
-    it("tests salaried and 300 sales", function(){
-    assert.strictEqual(computeSalesCommission(true, 300), 3);
+    it(" tests house cost is 100000", function(){
+        assert.strictEqual(calcDownpayment(100000), 7500);
     });
-    it("tests not salaried and 300 sales", function(){
-    assert.strictEqual(computeSalesCommission(false, 300), 6);
+    it(" tests house cost is 200000", function(){
+        assert.strictEqual(calcDownpayment(200000), 22500);
     });
-    it("tests salaried and 3500 sales", function(){
-    assert.strictEqual(computeSalesCommission(true, 3500), 70);
-    });
-    it("tests not salaried and 3500 sales", function(){
-    assert.strictEqual(computeSalesCommission(false, 3500), 105);
-    });
-   });
-describe("this function tests ComputSalesCommission", function(){
-    it("tests salaried and 400 sales",function(){
-    assert.strictEqual(computeSalesCommission(true,400),4);
-       });
-    it("tests not salried and 200 sales",function(){
-    assert.strictEqual(computeSalesCommission(false,200),0);
-       });
-   });
-   
+    
+});
--- sam.js
+++ sam.js
@@ -1,4 +1,5 @@
 "use strict";
+module.exports = {calcDownpayment};
 /**
  * this function calculate the down payment of house based on its 
  * cost.
@@ -17,22 +18,8 @@ function calcDownpayment(housecost){
      downpayment = 5000 + 0.1 *(housecost -200000);
      return downpayment;
 }
-const assert = require("assert");
-describe("cheak the house price and",function(){
-    it(" tests house cost is 40000", function(){
-        assert.strictEqual(calcDownpayment(40000), 2000);
-    });
-    it(" tests house cost is 50000", function(){
-        assert.strictEqual(calcDownpayment(50000), 2500);
-    });
-    it(" tests house cost is 100000", function(){
-        assert.strictEqual(calcDownpayment(100000), 7500);
-    });
-    it(" tests house cost is 200000", function(){
-        assert.strictEqual(calcDownpayment(200000), 22500);
-    });
-    
-});
+
+
 
 
 
