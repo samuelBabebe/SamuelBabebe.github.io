6842b521df04a28b22cec7e8fa1d57865146f0bc
samuelBabebe <samuel.abebe@miu.edu>
2021-09-02 00:03:10 -0500

    tests down payment with exported module

--- sam.js
+++ sam.js
@@ -1,23 +1,4 @@
-"use strict";
-module.exports = {calcDownpayment};
-/**
- * this function calculate the down payment of house based on its 
- * cost.
- * @param {number} housecost the cost of house. 
- * @returns {number} it returns the down payment.
- */
 
-
-function calcDownpayment(housecost){
-    let downpayment;
-    housecost<50000 ? downpayment = 0.05*housecost :
-    housecost>=50000 && housecost<100000 ? downpayment =
-     2500 + 0.1*(housecost-50000) :
-    housecost>=100000 && housecost<=200000 ? downpayment =
-     7500 + 0.15 *(housecost - 100000) :
-     downpayment = 5000 + 0.1 *(housecost -200000);
-     return downpayment;
-}
 
 
 
