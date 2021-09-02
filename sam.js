1d165ee27b0961a5a5f6f9b53537ef5e80f0078d
samuelBabebe <samuel.abebe@miu.edu>
2021-09-01 21:22:26 -0500

    Update sam.js

--- sam.js
+++ sam.js
@@ -1,9 +1,55 @@
-function calcDistance(x1,y1,x2,y2){
-    let distance = Math.round(Math.sqrt(Math.pow((x2-x1),2) +
-     Math.pow((y2-y1),2))*10)/10;
-    return distance;
-}
-console.log("expect 5 : ", calcDistance (0, 0, 5, 5));
-//here we were expecting to get 5 but when we calculate it, it is 7.1(rounded).
 
-SafeArray;
+"use strict";
+const assert = require("assert");
+/*eslint-disable*/
+/* global assert isVowel*/
+/* isVowel() that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false
+otherwise. */
+
+/**
+ * this function will accept a letter and decide if it
+ * is vowel or not
+ * 
+ * @param {string} input the letter
+ * @returns {Boolean} the output
+ */
+
+
+function isVowel(input){
+    
+    let vowel = ["a","i","e","o","u"];
+    for(let i = 0; i < vowel.length; i++){
+        if(input ===vowel[i]){
+            return true;
+        }
+       
+    }
+    return false;
+}
+
+
+describe("isVowel", function () {
+ it("a is vowel", function () {
+ assert.equal(isVowel("a"), true);
+ });
+ it("e is vowel", function () {
+ assert.equal(isVowel("e"), true);
+ });
+ it("i is vowel", function () {
+ assert.equal(isVowel("i"), true);
+ });
+ it("o is vowel", function () {
+ assert.equal(isVowel("o"), true);
+ });
+ it("u is vowel", function () {
+ assert.equal(isVowel("u"), true);
+ });
+ it("z is not vowel", function () {
+ assert.equal(isVowel("z"), false);
+ });
+ it("5 is not vowel", function () {
+ assert.equal(isVowel("5"), false);
+ });
+});
+/* i need to diable the eslint because it keep saying jsdoc missing 
+even if i already add the jsdoc and i get it as Index.html*/
