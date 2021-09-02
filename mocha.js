b87fe4ccbefa89903df339ce804c8ece1fc4be50
samuelBabebe <samuel.abebe@miu.edu>
2021-09-01 21:55:04 -0500

    i exported my file to test it on mocha

---
+++ mocha.js
@@ -0,0 +1,28 @@
+"use strict"
+const assert = require("assert");
+
+const functionmodule = require("./sam.js");
+const isVowel = functionmodule.isVowel;
+describe("cheaks if the input is vowel or not", function () {
+    it("a is vowel", function () {
+    assert.equal(isVowel("a"), true);
+    });
+    it("e is vowel", function () {
+    assert.equal(isVowel("e"), true);
+    });
+    it("i is vowel", function () {
+    assert.equal(isVowel("i"), true);
+    });
+    it("o is vowel", function () {
+    assert.equal(isVowel("o"), true);
+    });
+    it("u is vowel", function () {
+    assert.equal(isVowel("u"), true);
+    });
+    it("z is not vowel", function () {
+    assert.equal(isVowel("z"), false);
+    });
+    it("5 is not vowel", function () {
+    assert.equal(isVowel("5"), false);
+    });
+   });
--- sam.js
+++ sam.js
@@ -1,6 +1,6 @@
 
 "use strict";
-const assert = require("assert");
+module.exports = {isVowel}
 /*eslint-disable*/
 /* global assert isVowel*/
 /* isVowel() that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false
@@ -28,28 +28,4 @@ function isVowel(input){
 }
 
 
-describe("isVowel", function () {
- it("a is vowel", function () {
- assert.equal(isVowel("a"), true);
- });
- it("e is vowel", function () {
- assert.equal(isVowel("e"), true);
- });
- it("i is vowel", function () {
- assert.equal(isVowel("i"), true);
- });
- it("o is vowel", function () {
- assert.equal(isVowel("o"), true);
- });
- it("u is vowel", function () {
- assert.equal(isVowel("u"), true);
- });
- it("z is not vowel", function () {
- assert.equal(isVowel("z"), false);
- });
- it("5 is not vowel", function () {
- assert.equal(isVowel("5"), false);
- });
-});
-/* i need to diable the eslint because it keep saying jsdoc missing 
-even if i already add the jsdoc and i get it as Index.html*/
+
