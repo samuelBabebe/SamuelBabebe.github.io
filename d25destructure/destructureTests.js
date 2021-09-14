a9dfe2ed2c4f3597e0110bccef11f85e3f2a7e26
samuelBabebe <samuel.abebe@miu.edu>
2021-09-13 21:52:20 -0500

    destructure

---
+++ d25destructure/destructureTests.js
@@ -0,0 +1,20 @@
+"use strict"
+const assert = require("assert")
+const inport = require("./destructure.js")
+const topSalary = inport.topSalary;
+describe("topSalary", function() {
+    it("returns top-paid person", function() {
+      let salaries = {
+        "John": 100,
+        "Pete": 300,
+        "Mary": 250
+      };
+  
+      assert.equal( topSalary(salaries), "Pete" );
+    });
+  
+    it("returns null for the empty object", function() {
+        console.log("topsalary: ", topSalary({}) );
+      assert.strictEqual( topSalary({}), null);
+    });
+  });
