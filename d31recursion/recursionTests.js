4e836e69c79f403c68aca46fabc1e7c8fa15a7fb
samuelBabebe <samuel.abebe@miu.edu>
2021-09-14 22:11:27 -0500

    recursion

---
+++ d31recursion/recursionTests.js
@@ -0,0 +1,25 @@
+"use strict"
+
+const assert = require("assert");
+const input = require("./recursion.js");
+const sumTo = input.sum;
+const factorial = input.factorial;
+const fibonacci = input.fibonachi;
+
+describe("recursion tests", function () {
+    it("tests summing by recursion", function () {
+        assert.strictEqual(sumTo(5), 15);
+        assert.strictEqual(sumTo(10), 55);
+    });
+    it("tests factorial", function () {
+        assert.strictEqual(factorial(5), 120);
+        assert.strictEqual(factorial(10), 3628800);
+    });
+
+    it("tests fibonacci 10", function () {
+        assert.strictEqual(fibonacci(10), 55);
+    });
+    it("tests fibonacci 30", function () {
+        assert.strictEqual(fibonacci(30), 832040);
+    });
+});
